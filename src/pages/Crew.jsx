3aea4400192268c8a5d587a6c88bff1be0395052
Yuosef <www.yoyoadel@gmail.com>
2024-06-30 22:45:49 +0200

    fixed Crew.jsx chevrons bug

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -8,6 +8,14 @@ const Crew = ({crew}) => {
 
     const [crewIndex, setCrewIndex] = useState(2);
 
+    const incrementIndex = () => {
+        setCrewIndex(prevState => prevState + 1 > crew?.length - 1 ? 0 : prevState + 1);
+    }
+
+    const decrementIndex = () => {
+        setCrewIndex(prevState => prevState - 1 < 0 ? crew?.length - 1 : prevState - 1);
+    }
+
     return (
         <section className="crew bg-cover overflow-hidden flex items-center bg-[url(../assets/crew/background-crew-mobile.jpg)] md:bg-[url(../assets/crew/background-crew-tablet.jpg)] lg:bg-[url(../assets/crew/background-crew-desktop.jpg)] min-h-[100svh] pb-12 lg:p-0">
             <div className="container mt-40">
@@ -23,7 +31,7 @@ const Crew = ({crew}) => {
                         <CrewMember key={i} active={i === crewIndex} {...member}>
                             <div className="flex items-center justify-between">
                                 <button
-                                    onClick={() => setCrewIndex(prevState => (prevState - 1) % crew?.length)}
+                                    onClick={decrementIndex}
                                     className="text-white w-10 h-10 lg:hidden"
                                 >
                                     <img src={chevronLeft} alt="Chevron left icon" />
@@ -32,7 +40,7 @@ const Crew = ({crew}) => {
                                     {crew?.map((_, i) => <span className={i === crewIndex ? "active" : ""} key={i} onClick={() => setCrewIndex(i)}></span>)}
                                 </div>
                                 <button
-                                    onClick={() => setCrewIndex(prevState => (prevState + 1) % crew?.length)}
+                                    onClick={incrementIndex}
                                     className="text-white w-10 h-10 lg:hidden"
                                 >
                                     <img src={chevronRight} alt="Chevron right icon" />
@@ -42,13 +50,13 @@ const Crew = ({crew}) => {
                         );
                     })}
                     <button
-                        onClick={() => setCrewIndex(prevState => (prevState - 1) % crew?.length)}
+                        onClick={decrementIndex}
                         className="absolute hidden text-white w-10 h-10 -left-14 top-1/2 -translate-y-1/2 lg:block"
                     >
                         <img src={chevronLeft} alt="Chevron left icon" />
                     </button>
                     <button
-                        onClick={() => setCrewIndex(prevState => (prevState + 1) % crew?.length)}
+                        onClick={incrementIndex}
                         className="absolute hidden text-white w-10 h-10 -right-14 top-1/2 -translate-y-1/2 lg:block"
                     >
                         <img src={chevronRight} alt="Chevron left icon" />
